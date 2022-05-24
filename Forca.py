6a6483c3b32c6dbd03da2f135a2831c1c8a9f5d6
ETS Campinas (CaP/ETS) <ct67ca@bosch.com>
2022-05-24 16:49:51 -0300

    central jogos

--- Forca.py
+++ Forca.py
@@ -1,18 +1,35 @@
-from Menu import Menu
+from PyQt5 import uic, QtWidgets
+
 import random
 
-class Forca(Menu):
+class Forca():
     #Construtor
-    def __init__(self, dicas=[],letras_certas=[]):
-        #--Super da classe Menu
-        super(Forca, self).__init__(menu_principal, jogo_forca)
+    def __init__(self, palavras=[], dicas=[],letras_certas=[]):
         #--Instanciando variáveis
         self.dicas = dicas
         self.letras_certas = letras_certas
+        self.palavras = palavras
 
 
     #Métodos
     def sorteio(self):
-        #--Sorteia um número e retorna ele
+        #--Sorteia um número
         sorteio = random.randint(0, 3)
+        #--Retorna o número
         return sorteio
+
+    def palavra_secreta(self):
+        #--Cria lista de palavras
+        self.palavras = ['Pave'.upper(), 'Queijo'.upper(), 'Hamburguer'.upper()]
+        #--Palavra sorteada de acordo com a sua posição
+        palavra_sorteada = self.palavras[self.sorteio()]
+        #--Retorna a palavra sorteada
+        return palavra_sorteada
+
+    def dica_secreta(self, tela):
+        #--Cria lista de dicas
+        self.dicas = ['Sobremesa', 'Vem do leite', 'Tem dentro do lanche']
+        #--Dica sorteada de acordo com a sua posição
+        dica_sorteada = self.dicas[self.sorteio()]
+        #--Escreve a dica no label da tela
+        tela.dica.setText(dica_sorteada)
--- Menu.py
+++ Menu.py
@@ -1,8 +1,10 @@
+from Forca import Forca
 from PyQt5 import uic, QtWidgets
 
 class Menu():
     #Construtor
     def __init__(self, menu_principal=None, jogo_forca=None):
+        #Instanciando variáveis
         self.menu_principal = menu_principal
         self.jogo_forca = jogo_forca
 
@@ -11,13 +13,19 @@ class Menu():
     def iniciar(self):
         #--Chama a tela
         self.menu_principal = uic.loadUi("janelas/menu_inicial.ui")
+        #--Botao forca
+        self.menu_principal.botao_forca.clicked.connect(self.tela_forca)
         #--Exibe a janela
         self.menu_principal.show()
-        #--Botao forca
-        self.menu_principal.botao_forca.clicked.connect(self.ir_forca)
 
-    def ir_forca(self):
-        #--Chama a tela
+    def tela_forca(self):
+        # --Chama a tela
         self.jogo_forca = uic.loadUi("janelas/jogo_forca.ui")
-        #--Exibe a janela
+        # --Exibe a janela
         self.jogo_forca.show()
+        #--Fecha menu principal
+        self.menu_principal.close()
+        #--Instancia objeto da forca
+        forca = Forca()
+        #--Função de sortear palavra
+        forca.dica_secreta(self.jogo_forca)
