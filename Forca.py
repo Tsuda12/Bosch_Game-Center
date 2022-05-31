7bf921cd1e1e222df487ea91d787231d72977cdb
Tsuda12 <felipe.tsuda01@gmail.com>
2022-05-31 16:46:20 -0300

    commit

--- Forca.py
+++ Forca.py
@@ -1,12 +1,42 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
-import janelas.Forca_Tela
+#BIBLIOTECAS
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
+from random import randint
+import janelas_py.ForcaTela
 
-
-class Forca(janelas.Forca_Tela.Ui_Form):
+#CLASSE
+class Forca(janelas_py.ForcaTela.Ui_Form):
     #CONSTRUTOR
-    def __init__(self, carregar_tela=0):
-        #--Carrega a tela da forca
-        self.carregar_tela = uic.loadUi("janelas/forca.ui")
-        self.carregar_tela.show()
+    def __init__(self, sorteio=None, palavras=[], palavra_sorteada=None, dicas=[], dica_sorteada=None):
+        #--Variáveis
+        self.sorteio = sorteio
+        self.palavras = palavras
+        self.palavra_sorteada = palavra_sorteada
+        self.dicas = dicas
+        self.dica_sorteada = dica_sorteada
+        #--Carrega a ui da forca
+        self.janela = uic.loadUi("janelas_ui/forca.ui")
+        #--Chama métodos
+        self.resultado_sorteio = self.sortear_numero()
+        self.sortear_dica()
+        self.sortear_palavra()
+        self.ocultar_palavra()
+        #--Exibe a janela
+        self.janela.show()
 
 
+    #MÉTODOS
+    def sortear_palavra(self):
+        self.palavras = ['CARRO', 'MACACO', 'FIFA']
+        self.palavra_sorteada = self.palavras[self.resultado_sorteio]
+
+    def sortear_dica(self):
+        self.dicas = ['Meio de locomoção', 'Animal', 'Jogo eletrônico popular']
+        self.dica_sorteada = self.dicas[self.resultado_sorteio]
+        self.janela.lbl_txt_dica.setText(self.dica_sorteada)
+
+    def sortear_numero(self):
+        self.sorteio = randint(0, 2)
+        return self.sorteio
+
+    def ocultar_palavra(self):
+        self.janela.lbl_palavra_secreta.setText('*  ' * len(self.palavra_sorteada))
--- Main.py
+++ Main.py
@@ -1,30 +1,24 @@
-from PyQt5 import uic
-from PyQt5 import QtCore, QtGui, QtWidgets
+#BIBLIOTECAS
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
 from Forca import Forca
-import janelas.Menu_Tela
+import janelas_py.MenuTela
 
 
-class Central(janelas.Menu_Tela.Ui_MainWindow):
+#CLASSE PRINCIPAL
+class Central(janelas_py.MenuTela.Ui_MainWindow):
     #CONSTRUTOR
-    def __init__(self, MainWindow, janela_forca=None):
+    def __init__(self, MainWindow):
         #--Inicializa a janela
         self.setupUi(MainWindow)
-        #--Carregar tela
-        self.menu = uic.loadUi("janelas/menu_inicial.ui")
         #--Botões
         self.btn_forca.clicked.connect(self.abrir_forca)
-        self.btn_velha.clicked.connect(self.abrir_velha)
-
 
     #MÉTODOS
     def abrir_forca(self):
-        print("ABRIU FORCA")
-
-    def abrir_velha(self):
-        print("ABRIU VELHA")
+        self.forca = Forca()
 
 
-#MAIN
+#PROGRAMA
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
